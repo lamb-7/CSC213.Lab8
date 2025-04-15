419b8e860af3a7d0b02f450661abfc4b1435ecca
lamb-7 <lamb7@canisius.edu>
2025-04-15 10:58:25 -0400

    Completed TODOs and tests

--- src/main/java/com/example/reviews/App.java
+++ src/main/java/com/example/reviews/App.java
@@ -9,7 +9,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class App {
